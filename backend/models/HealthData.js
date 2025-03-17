82f9cd2935f614aeff5bfe2cc672d143441f662f
Prashant <150025514+Moxie-47@users.noreply.github.com>
2025-03-17 10:40:42 +0530

    Update HealthData.js

--- backend/models/HealthData.js
+++ backend/models/HealthData.js
@@ -4,7 +4,7 @@ const HealthDataSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   steps: { type: Number, required: true },
   heartRate: { type: Number, required: true },
-  date: { type: Date, default: Date.now }, // Automatically store the date
+  date: { type: Date, default: Date.now }, 
 });
 
 module.exports = mongoose.model("HealthData", HealthDataSchema);
