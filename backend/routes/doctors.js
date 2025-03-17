ef1228cbf3b6a22c0687da3708661ab568afa4bd
Prashant <150025514+Moxie-47@users.noreply.github.com>
2025-03-17 10:45:07 +0530

    Update doctors.js

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// ✅ Get all doctors
 router.get("/", async (req, res) => {
   try {
     const doctors = await User.find({ role: "doctor" }).select("_id name email");
