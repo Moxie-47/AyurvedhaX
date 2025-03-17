0a7ec6819fb7a3bd8ad3f1eb5843aafff6927d82
Prashant <150025514+Moxie-47@users.noreply.github.com>
2025-03-17 10:41:25 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["patient", "doctor"], default: "patient" }, // ✅ Add role field
+  role: { type: String, enum: ["patient", "doctor"], default: "patient" },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
